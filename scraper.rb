da83f6febbf25a53d0177ed0bf752c7c42adfc5d
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-09-29 23:58:32 -0400

    Filtering out kanji only from the articles.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require_relative 'hiragana'
+require_relative 'katakana'
 
 # Online development
 # sample = "http://headlines.yahoo.co.jp/hl?a=20130929-00000106-economic-bus_all"
@@ -14,6 +16,7 @@ class Scraper
   attr_reader :page
   attr_reader :title
   attr_accessor :text
+  attr_reader :length
   attr_reader :kanji_hash
 
   def initialize(url)
@@ -29,6 +32,7 @@ class Scraper
 
   end
 
+  # Get the article text from the URL
   def get_text
 
     # Get the text of the article from <p> tag of class="ynDetailText"
@@ -36,13 +40,16 @@ class Scraper
 
   end
 
+  # Create a hash which stores the kanji characters as a key and each character's frequency as a value
   def create_kanji_hash
 
     # Cycle through all characters in the string and add them to the hash if they are kanji
     @text.each_char do |char|
 
-      if is_a_kanji?(char)
+      # Check if the character in the article is a kanji
+      if is_kanji?(char)
 
+        # Add character to hash or increase the kanji count for an existing entry
         unless @kanji_hash[char]
 
           @kanji_hash[char] = 1
@@ -63,33 +70,69 @@ class Scraper
 
   end
 
-  def is_a_kanji?(char)
+  def is_kanji?(char)
 
     kanji = true
 
-    if !is_a_word_char?(char) ; return false ; end
+    # Check if character is a non-word character (.,=- etc.)
+    if !is_a_word_char?(char) ; kanji = false ; end
 
-    if is_romaji?(char) ; return false ; end
+    # Check if character is romaji (abcdefg etc.)
+    if is_romaji?(char) ; kanji = false ; end
 
-    kanji
+    # Check if the character is a hiragana
+    if is_hiragana?(char) ; kanji = false ; end
+
+    # Check if the character is a katakana
+    if is_katakana?(char) ; kanji = false ; end
+
+    # Check if the character is a number
+    if is_number?(char) ; kanji = false ; end
+
+    return kanji
+
+  end
+
+  def is_hiragana?(char)
+
+    hiragana = Hiragana.new
+
+    # Check if character is included in a list of hiragana
+    hiragana.list.include?(char)
+
+  end
+
+  def is_katakana?(char)
+
+    katakana = Katakana.new
+
+    # Check if character is included in a list of katakana
+    katakana.list.include?(char)
 
   end
 
   def is_romaji?(char)
 
+    # Matches if char is a letter from the English alphabet
     char =~ /[a-zA-Z]/
 
   end
 
   def is_a_word_char?(char)
 
+    # Matches if char is a non-word character (.,-= etc)
     char =~ /\p{Word}+/u
-    # string.gsub!("/[^a-zA-Z]/u", "")
+
+  end
+
+  def is_number?(char)
+
+    # Matches if char is a number
+    char =~ /[0-9]/
 
   end
 
 end
-
 
 
 scraper = Scraper.new(sample)
